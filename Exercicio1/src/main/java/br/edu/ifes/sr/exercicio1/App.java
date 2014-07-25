1671c10bafa08d858fc2cdcd6306fd3e47dd6645
Thanner Soares Silva <thannersoares@gmail.com>
2014-07-25 18:35:24 -0300

    Exercícios POO2. Padrãos Comportamentais.

--- Exercicio1/src/main/java/br/edu/ifes/sr/exercicio1/App.java
+++ Exercicio1/src/main/java/br/edu/ifes/sr/exercicio1/App.java
@@ -11,9 +11,6 @@ package br.edu.ifes.sr.exercicio1;
  */
 public class App {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Maquina maquina = new Maquina();
         maquina.getState().insereMoeda();
